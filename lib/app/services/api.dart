4ab69ae9d7795bb0ea6919ad7451c904ed4bc9ab
sampe <test@gmail.com>
2021-05-07 00:58:46 +0800

    imported http

--- lib/app/services/api.dart
+++ lib/app/services/api.dart
@@ -7,4 +7,12 @@ class API {
   final String apiKey;
 
   factory API.sandbox() => API(apiKey: APIKeys.ncovSandboxKey);
+
+  static final String host = 'ncov2019-admin.firebaseapp.com';
+
+  Uri tokenUri() => Uri(
+        scheme: 'https',
+        host: host,
+        path: 'token',
+      );
 }
---
+++ lib/app/services/api_service.dart
@@ -0,0 +1,9 @@
+import 'api.dart';
+import 'package:http/http.dart';
+
+class APIService {
+  APIService(this.api);
+  final API api;
+
+  Future<String> getAccessToken() {}
+}
